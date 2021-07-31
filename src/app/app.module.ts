0807d05db4a8c3a89f9d648c1e182d5f9ca6d5a0
rocoSittoni <roco.sittoni@protonmail.com>
2021-07-31 15:54:14 -0300

    swiper added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+
+// components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 // material modules
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,11 +21,8 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 
-// components
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
+// 3rd party library modules
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
@@ -31,6 +35,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
@@ -38,7 +43,8 @@ import { FooterComponent } from './components/footer/footer.component';
     MatMenuModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatOptionModule
+    MatOptionModule,
+    SwiperModule,
   ],
   exports: [
     MatToolbarModule,
@@ -50,7 +56,9 @@ import { FooterComponent } from './components/footer/footer.component';
     MatSelectModule,
     MatOptionModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { SwiperComponent } from "swiper/angular";
+import SwiperCore, { Navigation, Autoplay } from "swiper/core";
+
+SwiperCore.use([
+  Navigation,
+  Autoplay,
+]);
 
 @Component({
   selector: 'app-home',
@@ -7,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+  }
+
+  onSwiper(swiper: any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
+
+  @ViewChild("swiperRef", { static: false }) swiperRef?: SwiperComponent;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  breakpoints = {
+    640: { slidesPerView: 2, spaceBetween: 20 },
+    768: { slidesPerView: 4, spaceBetween: 40 },
+    1024: { slidesPerView: 4, spaceBetween: 50 }
+  };
+
+  breakPointsToggle: boolean = false;
+  breakpointChange() {
+    this.breakPointsToggle = !this.breakPointsToggle;
+    this.breakpoints = {
+      640: { slidesPerView: 2, spaceBetween: 20 },
+      768: { slidesPerView: 4, spaceBetween: 40 },
+      1024: { slidesPerView: this.breakPointsToggle ? 7 : 5, spaceBetween: 50 }
+    };
   }
 
 }
